62b4c03c66dd20a4cb1d4e0f718e8b036c16c421
Nejel <nejel@mail.ru>
2017-07-11 00:56:54 +0300

    update of decision

--- Day 0. Task 1.R
+++ Day 0. Task 1.R
@@ -13,13 +13,3 @@ cat(P , file = "", sep = "\n", fill = TRUE, labels = NULL, append = FALSE)
 
 
 
-//Python:
-import numpy as np
-from scipy import stats
-
-
-size = int(input())
-numbers = list(map(int, input().split()))
-print(np.mean(numbers))
-print(np.median(numbers))
-print(int(stats.mode(numbers)[0]))
