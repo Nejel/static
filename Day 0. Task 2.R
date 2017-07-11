d1fa51b051a3e18e4b2c41144bcc13ae4a4fae66
Nejel <nejel@mail.ru>
2017-07-11 21:39:12 +0300

    update day0 solutions, atom commit

--- Day 0. Task 2.R
+++ Day 0. Task 2.R
@@ -1,12 +1,16 @@
 //https://www.hackerrank.com/challenges/s10-weighted-mean/problem
 
-//Objective 
+//Objective
 //In the previous challenge, we calculated a mean. In this challenge, we practice calculating a weighted mean. Check out the Tutorial tab for learning materials and an instructional video!
 
 //Task 
 //Given an array, X, of N integers and an array, W, representing the respective weights of X's elements, calculate and print the weighted mean of X's elements. Your answer should be rounded to a scale of 1 decimal place (i.e., 12.3 format).
 
 //R:
-y <- c(1,2,3,4,5)
-w <- c(10,40,30,50,20)
-cat(weighted.mean(w,y))
+dataset <- file("stdin")
+open(dataset)
+q <- strsplit(readLines(dataset, warn=FALSE), split=" ")
+x <- as.numeric(q[[2]])
+w <- as.numeric(q[[3]])
+close(dataset)
+cat(format(round(crossprod(w, x) / sum(w), 1), nsmall=1))
