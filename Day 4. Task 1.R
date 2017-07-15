729d94ae1711aa7a738ecb1b74cd3416e67b3cc0
Nejel <nejel@mail.ru>
2017-07-16 00:45:27 +0300

    first commit fot today

--- Day 4. Task 1.R
+++ Day 4. Task 1.R
@@ -1,3 +1,9 @@
 
-a <- pbinom(3, 6, 0.5215311004784689)
+dataset <- file("stdin")
+open(dataset)
+q <- strsplit(dataset, split = " ")
+V <- as.numeric(q[[1]])
+close(dataset)
+#V = 1.09
+a <- 1-(pbinom(2, 6, (V/(V+1))))
 cat(format(round(c(a),3),nsmall=3))
