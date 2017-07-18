e371297a4344be2673958677b619c6245d8afd10
Nejel <nejel@mail.ru>
2017-07-18 23:18:33 +0300

    day 5 task 3

--- Day 5. Task 3. Normal distrib.R
+++ Day 5. Task 3. Normal distrib.R
@@ -4,6 +4,7 @@
 #In a certain plant, the time taken to assemble a car is a random variable, X , having a normal distribution with a mean of 20 hours and a standard deviation of 2 hours. What is the probability that a car can be assembled at this plant in:
 #Less than 19.5 hours?
 #Between 20 and 22 hours?
+#https://stat.ethz.ch/R-manual/R-devel/library/stats/html/Normal.html
 
 #Density, distribution function, quantile function and random generation for the normal distribution with mean equal to mean and standard deviation equal to sd.
 #Usage
