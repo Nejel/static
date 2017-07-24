896651cf48d8f231ba16ab1f515300acb670dc42
Nejel <nejel@mail.ru>
2017-07-24 22:55:28 +0300

    day 6 task 2

--- Day 6. Task 2. Central Limit theorem II.R
+++ Day 6. Task 2. Central Limit theorem II.R
@@ -2,10 +2,26 @@
 #https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1
 #https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1/tutorial
 
+#Density, distribution function, quantile function and random generation for the normal distribution with mean equal to mean and standard deviation equal to sd.
+#Usage
+
+#dnorm(x, mean = 0, sd = 1, log = FALSE)
+#pnorm(q, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
+#qnorm(p, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
+#rnorm(n, mean = 0, sd = 1)
+
+
 #My R solution
 
-a <- pnorm(250, mean = (100*2.4), sd = sqrt (100) * 2.0)
-cat(format(round(c(a),4),nsmall=4), sep = "\n")
+mean <- 500
+std <- 80
+n <- 100
+z <- 1.96
+a <- z * (std / sqrt(n))
+b <- mean - a
+c <- mean + a
+cat(format(round(c(b, c),2),nsmall=2), sep = "\n")
 
 #Result
-0.6915
+#484.32
+#515.68
