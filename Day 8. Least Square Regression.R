e4ca9d90b3d1e9d7f790ae36569c07d29f172052
Nejel <nejel@mail.ru>
2017-07-30 21:19:54 +0300

    Day 8. Least square regression

--- Day 8. Least Square Regression.R
+++ Day 8. Least Square Regression.R
@@ -1,22 +1,25 @@
 #Task
-#https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1
-#https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1/tutorial
-
-#Density, distribution function, quantile function and random generation for the normal distribution with mean equal to mean and standard deviation equal to sd.
-#Usage
-
-#dnorm(x, mean = 0, sd = 1, log = FALSE)
-#pnorm(q, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
-#qnorm(p, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
-#rnorm(n, mean = 0, sd = 1)
+#https://www.hackerrank.com/challenges/s10-least-square-regression-line/tutorial
 
 
 #My R solution
 
-a <- read.table("stdin",header=F, skip=1)
-dat <- t(a)
-M <- cor(dat[,1],dat[,2], method ="spearman")
-cat(round(M,3))
+data <- read.table("stdin",header=F, skip=0)
+dat <- t(data)
+x=dat[1,];
+y=dat[2,];
+m = lm(y~x)
+cat(round(predict(m, newdata=data.frame(x=80),se.fit=T)[[1]],digits=3))
 
 #Result
-#0.903
+#78.288
+
+#My R solution 2
+
+x= c(95,85,80,70,60)
+y= c(85,95,70,65,70)
+m = lm(y~x)
+cat(round(predict(m, newdata=data.frame(x=80),se.fit=T)[[1]],digits=3))
+
+#Result
+#78.288
